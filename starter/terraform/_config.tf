8f677286800712b1f2e16378af2df97dced26447
Stephen Akuffo <sa.akuffo@gmail.com>
2022-05-26 17:47:19 -0400

    ami change

--- starter/terraform/_config.tf
+++ starter/terraform/_config.tf
@@ -1,15 +1,15 @@
 terraform {
-   backend "s3" {
-     bucket = "udacity-terraform-course1"
-     key    = "terraform/terraform.tfstate"
-     region = "us-east-2"
-   }
- }
+  backend "s3" {
+    bucket = "saa-udacity-c1-terraform"
+    key    = "terraform/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
 
- provider "aws" {
-   region = "us-east-2"
+provider "aws" {
+  region = "us-east-2"
 
-   default_tags {
-     tags = local.tags
-   }
- }
+  default_tags {
+    tags = local.tags
+  }
+}
